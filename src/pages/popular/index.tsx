5dacbac7d7cd38739d5ae53450b4b15756d432b0
Lore Petriella <lorepetriella@gmail.com>
2023-03-05 14:02:47 -0300

    cambio sercios de populares, upcoming, top rated y now playing

--- src/pages/popular/index.tsx
+++ src/pages/popular/index.tsx
@@ -5,15 +5,22 @@ import { Col, Container, Row } from "react-bootstrap";
 import { BASE_IMG } from "../../constants";
 import { Movie } from "../../types";
 import { servicesMovies } from "../../services/movies";
+import { useSearchParams } from "react-router-dom";
 
 const PopularPage = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
-
+  const [page, setPage] = useState("");
+  const [totalPages, setTotalPages] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
   useEffect(() => {
     servicesMovies
-      .get("/movie/popular")
-      .then((data) => setMovies(data.results));
-  }, []);
+      .getPopular({ page: searchParams.get("page") || "1" })
+      .then((data) => {
+        setMovies(data.results);
+        setPage(data.page);
+        setTotalPages(data.total_pages);
+      });
+  }, [searchParams]);
 
   return (
     <Layout>
