5b276bfd4654918954fd584c3e3b97d342d78cf5
Lore Petriella <lorepetriella@gmail.com>
2023-03-23 00:16:28 -0300

    corrijo el tipo de imagen que muestra el banner

--- src/components/common/slider/index.tsx
+++ src/components/common/slider/index.tsx
@@ -16,7 +16,7 @@ const Slider: FC<Props> = ({ movies }) => {
           <Carousel.Item key={movie.id}>
             <img
               className="d-block w-100 slider-img"
-              src={`${BASE_IMG}${movie.poster_path}`}
+              src={`${BASE_IMG}${movie.backdrop_path}`}
               alt={movie.title}
             />
             <Carousel.Caption>
